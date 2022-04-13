7455425c5227b54ab829f38c0d47baf69035ba5b
danyalutsevich <luchevich31@gmail.com>
2022-04-13 21:25:28 +0300

    ImageSharp

--- GenerativeImages/Program.cs
+++ GenerativeImages/Program.cs
@@ -6,20 +6,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Threading;
+using System.Text;
 using System.Diagnostics;
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using Color = System.Drawing.Color;
 
 namespace GenerativeImages
 {
     internal class Program
     {
         public List<List<string>> imagesPaths;
+        public List<string> imageNames;
         public Random random;
         public string CurrentDir;
         private SemaphoreSlim semaphore;
+        int imageName = 0;
 
         public Program()
         {
             imagesPaths = new List<List<string>>();
+            imageNames = new List<string>();
             random = new Random();
             CurrentDir = Directory.GetCurrentDirectory();
             semaphore = new SemaphoreSlim(1);
@@ -29,25 +37,43 @@ namespace GenerativeImages
         {
             var GenIm = new Program();
 
-            GenIm.GenerateImagesPaths(1000);
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            GenIm.GenerateImagesPaths(100);
             await GenIm.GenerateImages();
-            GenIm.ArciveImages();
+            Console.WriteLine($"Done in {stopwatch.ElapsedMilliseconds}");
+
+            //GenIm.ArciveImages();
+
+            stopwatch.Stop();
 
         }
-
+        
         private void GenerateImagesPaths(int amount)
         {
             Console.Write("Generating Paths: ");
 
             int imagesCount = 0;
             int maxImages = 1;
+
             foreach (var d in Directory.EnumerateDirectories(CurrentDir + "\\layers"))
             {
                 foreach (var f in Directory.EnumerateFiles(d))
                 {
                     imagesCount++;
                 }
-                maxImages *= d.Length;
+                if (imagesCount != 0)
+                {
+                    maxImages *= imagesCount;
+                    imagesCount = 0;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Failed");
+                    Console.ResetColor();
+                    throw new Exception("Please remove empty folders");
+                }
             }
 
             if (amount > maxImages)
@@ -62,6 +88,7 @@ namespace GenerativeImages
             do
             {
                 List<string> imagePath = new List<string>();
+                StringBuilder imageName = new StringBuilder();
 
                 foreach (var d in Directory.EnumerateDirectories(Directory.GetCurrentDirectory() + "\\layers"))
                 {
@@ -69,13 +96,18 @@ namespace GenerativeImages
                     var list = files.ToList();
                     var index = random.Next(0, list.Count);
 
+                    imageName.Append(list[index]);
                     imagePath.Add(list[index]);
+
+
                 }
 
-                if (!imagesPaths.Contains(imagePath))
+                if (!imageNames.Contains(imageName.ToString()))
                 {
                     imagesPaths.Add(imagePath);
                 }
+
+
 
             } while (amount > imagesPaths.Count);
 
@@ -94,7 +126,9 @@ namespace GenerativeImages
 
             foreach (var l in imagesPaths)
             {
-                tasks.Add(GenerateImage(l));
+                imageName++;
+                //tasks.Add(GenerateImage(l, imageName));
+                tasks.Add(ImageSharpTest(l, imageName));
             }
 
             Task.WaitAll(tasks.ToArray());
@@ -103,9 +137,37 @@ namespace GenerativeImages
             Console.WriteLine("Done");
             Console.ResetColor();
         }
+    
+        private async Task ImageSharpTest(List<string> l,int imageName)
+        {
+            await Task.Run(() =>
+            {
+                SixLabors.ImageSharp.Image<Rgba32> image = SixLabors.ImageSharp.Image.Load<Rgba32>(l[0]);
 
-        int imageName = 0;
-        private async Task GenerateImage(List<string> l)
+                foreach (var s in l)
+                {
+                    var part = SixLabors.ImageSharp.Image.Load<Rgba32>(s);
+
+                    for (int x = 0; x < part.Width; x++)
+                    {
+                        for (int y = 0; y < part.Height; y++)
+                        {
+                            Rgba32 color = part[x, y];
+                            if (color.A != 0)
+                            {
+                                image[x, y] = color;
+                            }
+                        }
+                    }
+
+                }
+
+                image.SaveAsPng($"{CurrentDir}\\Output\\{imageName}.png");
+
+            });
+        }
+
+        private async Task GenerateImage(List<string> l, int imageName)
         {
             await Task.Run(() =>
             {
@@ -128,11 +190,8 @@ namespace GenerativeImages
                     }
 
                 }
-
-                semaphore.Wait();
-                imageName++;
+                
                 bmp.Save($"{CurrentDir}\\Output\\{imageName}.png");
-                semaphore.Release();
 
             });
         }
@@ -155,4 +214,7 @@ namespace GenerativeImages
             } while (true);
         }
     }
+
+
+
 }
